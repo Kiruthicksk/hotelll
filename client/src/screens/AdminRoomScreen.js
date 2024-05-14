f5c1ab9053ac60dcbbb85130ca6415169086f5d1
Kiruthicksk <159477943+Kiruthicksk@users.noreply.github.com>
2024-05-14 22:07:57 +0530

    Update AdminRoomScreen.js

--- client/src/screens/AdminRoomScreen.js
+++ client/src/screens/AdminRoomScreen.js
@@ -1,48 +1,54 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Table, Tag, Space } from "antd";
+import { Form, Input, InputNumber, Button, Select } from "antd";
+
+import Swal from "sweetalert2";
 
 import Loader from "../components/Loader";
 import Error from "../components/Error";
+const layout = {
+  labelCol: {
+    span: 4,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+function AdminAddRoomScreen() {
+  const { Option } = Select;
 
-function AdminRoomScreen() {
-  const [rooms, setRooms] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [room, setRoom] = useState({});
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
-  const columns = [
-    {
-      title: "roomid",
-      dataIndex: "_id",
-      key: "_id",
-    },
-    {
-      title: "name",
-      dataIndex: "name",
-      key: "name",
-    },
-    { title: "maxcount", dataIndex: "maxcount", key: "maxcount" },
-    { title: "phonenumber", dataIndex: "phonenumber", key: "phonenumber" },
-    { title: "rentperday", dataIndex: "rentperday", key: "rentperday" },
-    { title: "type", dataIndex: "type", key: "type" },
-  ];
+  const [form] = Form.useForm();
 
-  async function fetchMyData() {
+  const onFinish = async (values) => {
+    console.log(values);
     setError("");
     setLoading(true);
     try {
-      const data = (await axios.post("https://hotel-manis-lodge.onrender.com/api/rooms/getallrooms")).data;
-      setRooms(data);
+      const data = (await axios.post("https://hotel-manis-lodge.onrender.com/api/rooms/addroom", values)).data;
+      Swal.fire("Congratulations", "Your Room Added Successfully", "success");
+      form.resetFields();
     } catch (error) {
       console.log(error);
       setError(error);
+      Swal.fire("Opps", "Error:" + error, "error");
     }
-    setLoading(false);
-  }
 
-  useEffect(() => {
-    fetchMyData();
-  }, []);
+    setLoading(false);
+  };
+
+  const onReset = () => {
+    form.resetFields();
+  };
 
   return (
     <div className="row">
@@ -51,19 +57,128 @@ function AdminRoomScreen() {
       ) : error.length > 0 ? (
         <Error msg={error}></Error>
       ) : (
-        <>
-          <div className="col md-12">
-            <button className="btn btn-success" onClick={fetchMyData}>
-              Refresh
-            </button>
-          </div>
-          <div className="col-md-12">
-            <Table columns={columns} dataSource={rooms} />
-          </div>
-        </>
+        <div className="col-md-12">
+          <Form
+            {...layout}
+            form={form}
+            name="control-hooks"
+            onFinish={onFinish}
+          >
+            <Form.Item
+              name="name"
+              label="name"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="description"
+              label="description"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="maxcount"
+              label="maxcount"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+            >
+              <InputNumber min={1} defaultChecked={1} />
+            </Form.Item>
+            <Form.Item
+              name="phonenumber"
+              label="phonenumber"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="rentperday"
+              label="rentperday"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+            >
+              <InputNumber min={1} defaultChecked={1} />
+            </Form.Item>
+            <Form.Item
+              name="imageurl1"
+              label="imageurl1"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="imageurl2"
+              label="imageurl2"
+              rules={[
+                {
+                  //required: true,
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="imageurl3"
+              label="imageurl3"
+              rules={[
+                {
+                  //required: true,
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="type"
+              label="type"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+            >
+              <Select placeholder="Select a room type" allowClear>
+                <Option value="delux">Delux</Option>
+                <Option value="non-delux">Non-Delux</Option>
+              </Select>
+            </Form.Item>
+            <Form.Item {...tailLayout}>
+              <Button type="success" htmlType="submit">
+                Add
+              </Button>
+              <Button type="danger" htmlType="button" onClick={onReset}>
+                Reset
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
       )}
     </div>
   );
 }
 
-export default AdminRoomScreen;
+export default AdminAddRoomScreen;
